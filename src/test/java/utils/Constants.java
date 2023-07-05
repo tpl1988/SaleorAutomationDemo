79a4f17952795c2e3bd4f5d012c36067b20f13e6
LeTran <letran@kms-technology.com>
2023-07-05 15:23:51 +0700

    use local saleor-platform

--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -1,8 +1,8 @@
 package utils;
 
 public class Constants {
-	public static final String DASHBOARD_URL = "http://localhost:9000/";
-	public static final String API_URL = "http://localhost:8000/graphql/";
+	public static final String DASHBOARD_URL = "http://10.5.0.57:9000/";
+	public static final String API_URL = "http://10.5.0.57:8000/graphql/";
 	public static final String EMAIL = "admin@example.com";
 	public static final String PASSWORD = "admin";
 }
