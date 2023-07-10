192d09e79e9b5feceac3837b228e730698d80368
LeTran <letran@kms-technology.com>
2023-07-10 08:59:41 +0700

    stablize TS

--- src/test/java/test/MainTest.java
+++ src/test/java/test/MainTest.java
@@ -25,7 +25,7 @@ public class MainTest extends BaseTest implements IExecutionListener {
 		passwordField.sendKeys(Keys.DELETE);
 		passwordField.sendKeys(Constants.PASSWORD);
 		signInButton.click();
-		Thread.sleep(3000);
+		Thread.sleep(2000);
 		Assert.assertTrue(driver.findElement(By.xpath("//div[@data-test-id='welcome-header']")).isDisplayed());
 	}
 
